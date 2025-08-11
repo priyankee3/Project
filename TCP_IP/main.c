7dd66877286f16cc34b095f6197458d9212d87d0
Priyank Prajapati <ee3@pumpacademy.in>
2025-08-11 18:06:59 +0530

    Update TCP_IP code

--- TCP_IP/main.c
+++ TCP_IP/main.c
@@ -7,47 +7,63 @@ int main()
 	s32 sockfd,connfd,len;	// scokfd is  for file descriptor for Socket
 	struct sockaddr_in servaddr, cliaddr;	// Structure for adding information about ip address, port no, protocol type
 	s8 buff[128];
+	s8 *e = NULL;
 
 	// Creating socket for communication
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if(sockfd < 0 ){
-		printf("Socket creation failed......\n");
-		return 0;
+	if(sockfd < 0)
+	{
+		perror("Socket Creation");
+		exit(EXIT_FAILURE);
 	}
 
-	// Assign IP Port
-	servaddr.sin_family = AF_INET;	//IPv4
-	servaddr.sin_port = htons(5000);	// assigning port number
-	//servaddr.sin_addr.s_addr = inet_addr("192.168.1.110");	//Assiging IP address
-	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);	//Assiging IP address
+	// Assign IP and PORT
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port = htons(5000);
 
-	// Binding Socket with IP 
-	if(bind(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) != 0){
-		printf("Binding socket failed.....\n");
-		return 0;
+	// Bind Socket
+	if(bind(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) != 0)
+	{
+		perror("Bind status");
+		close(sockfd);
+		exit(EXIT_FAILURE);
 	}
 
-	// Server is ready to listen to 5 connection
-	if(listen(sockfd, 5)){
-		printf("Failed to connection.....\n");
-		return 0;
+	// Listen
+	if(listen(sockfd, 5) != 0)
+	{
+		perror("Listen status");
+		close(sockfd);
+		exit(EXIT_FAILURE);
 	}
-	
 
-	// Accept the data packet from client and verification
-	len = sizeof(cliaddr);	
+	printf("Server Listening on port 5000...\n");
+
+	// Accept one client 
+	len = sizeof(cliaddr);
 	connfd = accept(sockfd, (struct sockaddr*)&cliaddr, &len);
-	if( connfd < 0 ){
-		printf("Server acception failed.....\n");
-		return 0;
+	if(connfd < 0)
+	{
+		perror("Accept failed");
+		close(sockfd);
+		exit(EXIT_FAILURE);
 	}
 
-	read(connfd, buff, sizeof(buff));	// Reading data from device
-	printf("In server data received %s\n", buff);	
-	bzero(buff, sizeof(buff));
-	strcpy(buff,"Done");
-	write(connfd, buff, sizeof(buff));	// Writing data from device
+	printf("Client Connected: %s:%d\n", inet_ntoa(cliaddr.sin_addr),ntohs(cliaddr.sin_port));
+	
+	// Read and Write with client
+	s32 n;
+	while(1)
+	{
+		n = read(connfd, buff, sizeof(buff)-1);
 
-	close(connfd);	// closing connection with device
-	close(sockfd);	// closing socket for commuincation
+		printf("Received String: %s\n", buff);
+
+		strcpy(buff,"DONE");
+		write(connfd, buff, sizeof(buff));
+	}
+
+	close(connfd);
+	close(sockfd);
 }
