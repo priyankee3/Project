ca0cd0774ed1b4db58ffe0ea8f68390c145719fe
Priyank Prajapati <ee3@pumpacademy.in>
2025-08-29 17:22:10 +0530

    MQTT Testing

---
+++ MQTT/header.h
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdint.h>
+#include<stdlib.h>
+#include<string.h>
+#include<rabbitmq-c/amqp.h>
+#include<rabbitmq-c/tcp_socket.h>
+// #include "utils.h"
+
+// typedef
+typedef char s8;
+typedef unsigned char u8;
+typedef short int s16;
+typedef unsigned short int u16;
+typedef int s32;
+typedef unsigned int u32;
+typedef float f32;
+typedef double d64;
